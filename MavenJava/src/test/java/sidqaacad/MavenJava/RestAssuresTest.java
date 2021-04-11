4819a49cec97939ceaaa3540a444ad209c94d36f
Sidlearning <uattestthree@gmail.com>
2021-04-11 15:47:32 +0100

    First Maven Commit

---
+++ MavenJava/src/test/java/sidqaacad/MavenJava/RestAssuresTest.java
@@ -0,0 +1,12 @@
+package sidqaacad.MavenJava;
+
+public class RestAssuresTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("RestAssuredtest");
+
+	}
+
+}
---
+++ MavenJava/src/test/java/sidqaacad/MavenJava/SeleniumTest.java
@@ -0,0 +1,11 @@
+package sidqaacad.MavenJava;
+
+public class SeleniumTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("RestAssuredtest");
+	}
+
+}
